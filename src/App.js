e77d971a8e37046f535f97f62763bf8331420b82
Sohaib Aumer <sohaibaumer@hotmail.com>
2022-04-18 21:34:55 +0500

    add Account Page and update Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ErrorPage from './pages/Error';
+import AccountPage from './pages/Account';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} errorElement={<ErrorPage />} />
+        <Route path="/account" element={<AccountPage />} errorElement={<ErrorPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </BrowserRouter>
